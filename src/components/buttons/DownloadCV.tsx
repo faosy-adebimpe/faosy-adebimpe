d9e711a3afffd09a4cf961d01358604989d7f30c
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-07-24 04:56:44 -0700

    rename sidebar image to png

--- src/components/buttons/DownloadCV.tsx
+++ src/components/buttons/DownloadCV.tsx
@@ -2,7 +2,7 @@ const DownloadCV = () => {
     return (
         <div className="flex flex-col sm:flex-row gap-2 items-center sm:items-start">
             <a
-                href="/full-stack-developer-faosy-adebimpe-cv.pdf"
+                href="/cvs/faosy-adebimpe-software-engineer.pdf"
                 download
                 className="inline-block px-4 py-2 rounded-md bg-[var(--theme-color)] text-white font-medium shadow-md hover:scale-105 hover:shadow-lg transition-all duration-200 border border-transparent hover:border-[var(--theme-color)] focus:outline-none focus:ring-2 focus:ring-[var(--theme-color)] text-sm"
             >
